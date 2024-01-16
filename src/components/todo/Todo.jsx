8cbe3683e5b5e75d647bcf713962297c049768df
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2024-01-16 16:52:32 +0100

    add to do list function working now

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -5,35 +5,65 @@ import data from "../../data/data.json";
 
 export default function Todo() {
   const [allItems, setAllItems] = useState(data.todos);
+  const [newInput, setNewInput] = useState("");
 
-  function toggle(id){
-    setAllItems(prevItems => {
-      return (
-        prevItems.map((allItems) => {
-          return allItems.id === id ? {...allItems, done: !allItems.done} : allItems
-        })
-      )
+  function addToList() {
+    setAllItems((prevItems) => {
+      return [
+        ...allItems,
+        {
+          id: allItems.length,
+          value: newInput,
+          done: false
+        }
+      ]
     })
   }
 
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      addToList()
+    }
+  }
+
+  function toggle(id) {
+    setAllItems((prevItems) => {
+      return prevItems.map((allItems) => {
+        return allItems.id === id
+          ? { ...allItems, done: !allItems.done }
+          : allItems;
+      });
+    });
+  }
+
   const todoElement = allItems.map((item) => (
-    <TodoItem 
-    key={item.id}
-    done={item.done}
-    text={item.value}
-    handleClick={() => toggle(item.id)}
+    <TodoItem
+      key={item.id}
+      done={item.done}
+      text={item.value}
+      handleClick={() => toggle(item.id)}
     />
-  ))
+  ));
 
   return (
     <div>
       <div className="todo">
-        <input
-          className="todo-input"
-          type="text"
-          placeholder="Create a new todo..."
-          name="input"
-        />
+        <ul>
+          <li>
+            <input
+              className="todo-input"
+              type="text"
+              placeholder="Create a new todo..."
+              name="newItem"
+              onChange={(event) => {
+                setNewInput(event.target.value)
+              }}
+              onKeyDown={handleKeyDown}
+              value={newInput}
+            />
+            <span></span>
+          </li>
+        </ul>
       </div>
       {todoElement}
     </div>
