d4a536725ea88d7f8b7f1e1800f343f9b57a83ef
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2024-01-30 15:35:49 +0100

    make the clear completed filter

--- src/components/anTodoFilter/TodoFilter.jsx
+++ src/components/anTodoFilter/TodoFilter.jsx
@@ -1,40 +1,48 @@
 import React from "react";
-import { clsx } from 'clsx';
+import { clsx } from "clsx";
 import "../todo/todo.scss";
-import "./todo-filter.scss"
+import "./todo-filter.scss";
 
 const options = [
   {
-    name: 'all',
-    displayName: 'All'
+    name: "all",
+    displayName: "All",
   },
-  
-  {
-    name: 'active',
-    displayName: 'Active'
-  },
-  
-  {
-    name: 'completed',
-    displayName: 'Completed'
-  },
-  
-]
 
-export default function TodoFilter({filter, onFilterChange, items}) {
+  {
+    name: "active",
+    displayName: "Active",
+  },
+
+  {
+    name: "completed",
+    displayName: "Completed",
+  },
+];
+
+export default function TodoFilter({
+  filter,
+  onFilterChange,
+  items,
+  deleteItem,
+}) {
   return (
     <div className="todo-filter">
       <p>{items.length} items left</p>
-      <div className="todo-filter__buttons">
-        {
-          options.map((button) => {
+        <div className="todo-filter__buttons">
+          {options.map((button) => {
             return (
-              <button key={button.name} onClick={() => onFilterChange(button.name)} className={clsx(filter === button.name && "active")}>{button.displayName}</button>
-            )
-          })
-        }
-      </div>
-      <button>Clear Completed</button>
+              <button
+                key={button.name}
+                onClick={() => onFilterChange(button.name)}
+                className={clsx(filter === button.name && "active")}
+              >
+                {button.displayName}
+              </button>
+            );
+          })}
+        </div>
+        <button onClick={deleteItem}>Clear Completed</button>
     </div>
   );
 }
